1c320b474934642c26c4a60ae6b75d5815d32796
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2022-04-21 09:59:48 +0200

    Initial

---
+++ src/app.py
@@ -0,0 +1,26 @@
+"""
+Mini Flask API mini file
+"""
+
+from flask import Flask, Response
+import json
+
+def create_app():
+    """
+    Factory function for flask application
+    :return: app object
+    """
+    app = Flask(__name__)
+
+    @app.get('/api/v1.0/first')
+    def first_get():
+        data = {
+            'name': 'Jane',
+            'age': 34
+        }
+
+        return Response(json.dumps(data), 200, content_type='application/json')
+    return app
+
+if __name__ == '__main__':
+    create_app().run()
